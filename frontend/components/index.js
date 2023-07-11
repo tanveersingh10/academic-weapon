6778989605889b9152664b6693facace2dee967d
Tanveer Singh <tanveersengh@gmail.com>
2023-07-11 15:14:28 +0800

    testing 4

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -2,6 +2,7 @@ import BackButton from "./BackButton";
 import BottomNavigator from "./BottomNavigator";
 import Card from "./Card";
 
+//to make exports more modular
 export {
     BackButton,
     BottomNavigator,
